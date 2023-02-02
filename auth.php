6bb13c3aaeeb30b152f99fb2e5254ca34861312d
brian_ogola <brianochiengogola78@gmail.com>
2023-02-02 23:20:14 +0300

    fixed bugs

--- auth.php
+++ auth.php
@@ -1,7 +1,8 @@
+
+
 <?php
 session_start();
 include ('dbh.php');
-
 if(isset($_POST['username']) && isset($_POST['pswd'])){
     $username = $_POST['username'];
     $pswd = $_POST['pswd'];
@@ -11,7 +12,7 @@ if(isset($_POST['username']) && isset($_POST['pswd'])){
     }else if(empty($password)){
         header("Location: login.php?error=Password is required";)
     }else{
-        $statement = $databaseConnection->prepare("SELECT * FROM login.php WHERE uername=?");
+        $statement = $databaseConnection->prepare("SELECT * FROM login.php WHERE username=?");
         $statement->execute([$username]);
 
         if($statement->rowCount()===1){
