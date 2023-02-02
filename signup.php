752e6b6712a89b8611e4913acff4aac6bbc1d360
brian_ogola <brianochiengogola78@gmail.com>
2023-02-02 22:21:15 +0300

    fixed bugs

--- signup.php
+++ signup.php
@@ -13,59 +13,62 @@
 <?php
 $Email=$Paswd='';
 $errors=array('Email'=>'','Paswd'=>'');
-if(isset($_POST['sign'])){
-    //checking for username validation
+if(isset($_POST['save'])){
+    
+    //checking for email validation
     if(empty($_POST['Email'])){
         $errors['Email']='Email cannot be empty<br/>';
     }else{
         $Email=$_POST['Email'];
         if(!filter_var($Email, FILTER_VALIDATE_EMAIL)){
-            $errors['Email']='Email must be a valid address';
+            $errors['Email']='email must be a valid address';
         }
-
     }
-     //checking for password validation
-     if(empty($_POST['Paswd'])){
-        $errors['password'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ]='lastname cannot be empty<br/>';
+    //checking for password validation
+    if(empty($_POST['Paswd'])){
+        $errors['Paswd']='Password cannot be empty<br/>';
     }else{
         $Paswd=$_POST['Paswd'];
-        if(!preg_match('/^[a-zA-Z\s]+$/',$Paswd)){
-            $errors['password']='password must be letters and spaces only';
-        }
+        $uppercase = preg_match('@[A-Z]@', $Paswd);
+$lowercase = preg_match('@[a-z]@', $Paswd);
+$number    = preg_match('@[0-9]@', $Paswd);
+$specialChars = preg_match('@[^\w]@', $Paswd);
 
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($Paswd) < 8) {
+    echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+}else{
+    echo 'Strong password.';
+}
     }
     if(array_filter($errors)){
-        //echo 'There are Errors in the form';
+        //echo 'there are errors in the form';
     }else{
-        //echo 'No errors in the form';
+        //echo 'no errors in the form';
         /*$statement = $databaseConnection ->prepare(
-            "INSERT INTO details(firstname,lastname,email,course)
-            VALUES($firstname,$lastname,$email,$course)");
-            $statement ->execute();
-        ) */
-    }
-    try
-        {
-            $query = "INSERT INTO signuptb(Email, Paswd) VALUES(:Email, :Paswd);";
-            $query_run = $databaseConnection -> prepare($query);
-            $data =[
-                ":Email" => $Email,
-                ":Paswd" => password_hash($Paswd, PASSWORD_BCRYPT)
-                
-            ];
-            $query_execute = $query_run -> execute($data);
-            if($query_execute){
-                echo '<script> alert("Signup successful")</script>';
-            }else{
-                echo '<script> alert("Something went wrong, please try again..")</script>';
+            "INSERT INTO sample(firstname, lastname, email, course)
+            VALUES ($firstname, $lastname, $email, $course)");
+            $statement ->execute();*/
+            try
+            {
+                $query = "INSERT INTO signupth (Email, Paswd) VALUES (:email,:password)";
+                $query_run = $databaseConnection ->prepare($query);
+                $data = [
+                    ':email' => $Email,
+                    ':password' => $Paswd,
+                ];
+                $query_execute = $query_run-> execute($data);
+                if($query_execute){
+                    echo '<script> alert("Data added Successfully")</script>';
+                }else{
+                    echo '<script> alert("Data NOT added Successfully")</script>';
+                }
+            }catch(PDOException $err){
+                echo $err->getmessage();
             }
-            
-        }catch(PDOException $err){
-            echo $err -> getMessage();
-        }
-    
-    
+    }
+
 }
+
 ?>
 <body>
 <div class="col-md-4 offset-md-4">
@@ -87,7 +90,7 @@ if(isset($_POST['sign'])){
                  <div class='text-danger'><?php echo $errors['Paswd']?></div>
 
               </div>
-              <button name="sign" class="btn btn-primary">Sign up</button>
+              <button name="save" class="btn btn-primary">Sign up</button>
       </form>
 </div>
 </body>
