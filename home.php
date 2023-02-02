5d3f46f8dd3a0d1ee5a5b652fef313aefc881432
brian_ogola <brianochiengogola78@gmail.com>
2023-02-03 00:03:30 +0300

    services section

--- home.php
+++ home.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form</title>
-    <link rel="stylesheet" href="./bootstrap-4.6.2-dist/css/bootstrap.min.css ">
-   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="assets/css/animated-services.css">
 </head>
 <body>
 <div class="container text-center mt-4">
@@ -50,6 +51,51 @@
     </div>
   </div>
 </div>
-</body>
+<section id="services" class="services">
+        <div class="container-md section-title">
+            <div class="text-center">
+                <h5 class="d-inline-block justify-content-lg-center" style="background-color: #e2d6b5;width: 120px;margin-left: 0px;border-radius: 30px;padding: 5px;margin-top: 20px;color: #ffffff;font-size: 18px;">SERVICES</h5>
+                <h2 style="color: #75aadb;">We Offer Awesome<span style="color: #d12d33;"><strong>&nbsp;Services</strong></span></h2>
+                <p class="d-inline-block" style="width: 50%;"><strong>We are a software registered and licensed company. We design and implement web services to our esteemed clients.&nbsp;</strong>&nbsp; &nbsp;&nbsp;</p>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="text-center icon-box">
+                        <div class="icon"><i class="fab fa-dribbble" style="margin-bottom: 15px;"></i>
+                            <h4 class="title">Software Design<a href="#"></a></h4>
+                            <p class="description">We design and implement software tools according to the requirements of our clients.&nbsp; &nbsp; &nbsp;&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="text-center icon-box">
+                        <div class="icon"><i class="fab fa-renren" style="margin-bottom: 15px;"></i>
+                            <h4 class="title">Web Hosting<a href="#"></a></h4>
+                            <p class="description">We offer Website hosting services to at affortable subscriptions according to our client's plan&nbsp; &nbsp; &nbsp;&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="text-center icon-box">
+                        <div class="icon"><i class="fab fa-codepen" style="margin-bottom: 15px;"></i>
+                            <h4 class="title">System Maintenance<a href="#"></a></h4>
+                            <p class="description">We also offer maintenance services for your websites and systems, We also implement seo to your websites.&nbsp; &nbsp; &nbsp;&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="text-center icon-box">
+                        <div class="icon"><i class="fab fa-telegram" style="margin-bottom: 15px;"></i>
+                            <h4 class="title">Support<a href="#"></a></h4>
+                            <p class="description">We offer a 24 hour support to systemes designed, implemented and deployed by SavanahDevs&nbsp; &nbsp; &nbsp;&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.bundle.min.js"></script>
 <?php include("footer.php")?>
+</body>
 </html>
